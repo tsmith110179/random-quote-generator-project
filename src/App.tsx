a4f18c91258bcb62776eb712754f6e1ea62d135b
unknown <tsmith110179@yahoo.com>
2024-11-19 15:18:13 -0600

    second try

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import quotes from './assets/quotes.json';
-import { FaTwitter, FaQuoteLeft, FaQuoteRight} from 'react-icons/fa';
+import { FaXTwitter, FaQuoteLeft, FaQuoteRight} from 'react-icons/fa';
 import './App.css';
 import { FaXTwitter } from 'react-icons/fa6';
 
